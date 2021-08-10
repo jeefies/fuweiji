0f3bda579200239e3a279e0117bece227c6ffd13
jeefy <jeefy163@163.com>
2021-08-10 15:40:54 +0800

    new smp ip set api

--- app/static/smpip/getip.py
+++ app/static/smpip/getip.py
@@ -8,4 +8,5 @@ with open('/tmp/iplog.log') as f:
     c = f.read()
     data = ' '.join(re.findall('inet (192\.168\.\d+\.\d+)', c))
     #req.post('http://localhost:5000/smpip', {'ip': data})
-    req.post('http://fuweiji.pythonanywhere.com/smpip', {'ip': data})
+    # new version, update api place
+    req.post('http://fuweiji.pythonanywhere.com/smp/ip', {'ip': data})
