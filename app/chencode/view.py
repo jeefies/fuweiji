e437b8105bb7ac2a47f862e2ced052148a43960c
jeefies <jeefy163@163.com>
2022-01-02 20:12:54 +0800

    add chencode gui module

--- app/chencode/view.py
+++ app/chencode/view.py
@@ -11,4 +11,33 @@ from .. import _chencode as ccd
 
 @bp.route('/')
 def index():
-    return ccd.encode('草泥马')
+    return render_template('chencode/index.html')
+
+@bp.route('/encode', methods=["GET", "POST"])
+def encode():
+    return ccd.encode(req.values.get("ctx", ''))
+
+@bp.route('/decode', methods=["GET", "POST"])
+def decode():
+    return ' '.join(ccd.decode(req.values.get("ctx", '')))
+
+@bp.route('/result', methods=["POST"])
+def result():
+    ctx = req.values.get("ctx", '').strip()
+    code = req.values.get("code")
+
+    sec = None
+    ori = None
+    if code == 'en':
+        try:
+            sec = ccd.encode(ctx)
+            ori = ctx
+        except Exception as e:
+            return render_templace("error.html", e = str(e))
+    elif code == 'de':
+        try:
+            sec = ctx
+            ori = ''.join(ccd.decode(ctx))
+        except Exception as e:
+            return render_template('error.html', e = str(e))
+    return render_template("chencode/result.html", origin = ori, secret = sec)
